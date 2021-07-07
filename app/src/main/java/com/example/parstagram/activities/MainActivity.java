ea72ad5c6eb718fcee91a198fc1fd73d997e198a
Meghan Longtin Lin <mlin2024@fb.com>
2021-07-07 19:21:36 -0400

    Implemented timeline and compose fragments

--- app/src/main/java/com/example/parstagram/activities/MainActivity.java
+++ app/src/main/java/com/example/parstagram/activities/MainActivity.java
@@ -1,31 +1,59 @@
 package com.example.parstagram.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.parstagram.R;
+import com.example.parstagram.fragments.ComposeFragment;
+import com.example.parstagram.fragments.TimelineFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
     public Toolbar mainToolbar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
         mainToolbar = findViewById(R.id.mainToolbar);
         setSupportActionBar(mainToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.action_compose:
+                    fragment = new ComposeFragment();
+                    break;
+                case R.id.action_timeline:
+                default:
+                    fragment = new TimelineFragment();
+            }
+            fragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit();
+            return true;
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_timeline);
     }
 
     // Starts an intent to go to the loginOrSignup activity
